0359ede5be65e406ae959319329120bc7ec425ea
Gabriellvs60 <gabriellvs60@gmail.com>
2019-11-12 06:06:19 -0300

    Criando Menu com Sidebar
    Criando Menu com Sidebar

---
+++ sidebar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from '../src/components/Sidebar';
+
+function App() {
+  return ( 
+       <Sidebar/>
+  );
+}
+
+export default App;
