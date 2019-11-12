0b3deb1168f7afb3f0fed978639219764b5c090b
Gabriellvs60 <gabriellvs60@gmail.com>
2019-11-12 17:30:20 -0300

    Criação de projeto com antDesign
    Criação de projeto protótipo com antDesign

--- antdesign/src/App.js
+++ antdesign/src/App.js
@@ -3,59 +3,58 @@ import './App.css';
 //Importing static imgs
 import avatarIcon from '../src/static/imgs/lion.svg';
 //Imports ANTD
-import { Layout, Avatar, Menu, Icon, Breadcrumb} from 'antd';
+import { Layout, Avatar, Menu, Icon, Breadcrumb } from 'antd';
 import { Typography } from 'antd';
 
-const { Header, Footer, Sider, Content} = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 const { Title } = Typography;
 const { SubMenu } = Menu;
 
 function App() {
   return (
     <div className="App">
-     <Layout>
-     <Header className="header-content">
-       <Avatar className="avatar" src={avatarIcon} />
-       <Title className="title-header" level={3}>Design Studing</Title>
-     </Header>
-     <Layout>
-       <Sider className="sider">
-        <Menu
-        defaultSelectedkeys={['Dashboard']}
-        mode='inline'
-        > 
-          <Menu.Item key='Dashboard'>
-            Dashboard
-          </Menu.Item>
-          
-          <SubMenu
-          title={
-            <span>
-            <Icon type="mail" />
-            <span>About Us</span>
-            </span>
-          }
-          >
-            <Menu.ItemGroup key="AboutUs" title='Country'>
-                <Menu.Item key='location1'>Location 1</Menu.Item>
-                <Menu.Item key='location2'>Location 2</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-        </Menu>
-       </Sider>
-     <Layout>
-        <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 580 }}>Content</div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design Layout</Footer>
+      <Layout>
+        <Header className="header-content">
+          <Avatar className="avatar" src={avatarIcon} />
+          <Title className="title-header" level={3}>Design Studing</Title>
+        </Header>
+        <Layout>
+          <Sider className="sider">
+            <Menu
+              defaultSelectedkeys={['Dashboard']}
+              mode='inline'
+            >
+              <Menu.Item key='Dashboard'>
+                Dashboard
+            </Menu.Item>
+              <SubMenu
+                title={
+                  <span>
+                    <Icon type="mail" />
+                    <span>About Us</span>
+                  </span>
+                }
+              >
+                <Menu.ItemGroup key="AboutUs" title='Country'>
+                  <Menu.Item key='location1'>Location 1</Menu.Item>
+                  <Menu.Item key='location2'>Location 2</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{ padding: '0 50px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ background: '#fff', padding: 24, minHeight: 580 }}>Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design Layout</Footer>
+          </Layout>
         </Layout>
       </Layout>
-    </Layout>
-  </div>
+    </div>
   );
 }
 
---
+++ antdesignprototype/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Protótipo de Design</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
