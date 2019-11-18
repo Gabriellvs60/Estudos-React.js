d401b12586eb8e0f221a5d618745b42fea86f821
Gabriellvs60 <gabriellvs60@gmail.com>
2019-11-18 17:14:26 -0300

    Ajustes na navbar
    Consegui centralizar o button de uma forma interessante na header, agora falta alinhar a ele a pagination

--- antdesignprototype/src/components/Layout/Header/Header.js
+++ antdesignprototype/src/components/Layout/Header/Header.js
@@ -6,19 +6,26 @@ class Header extends Component {
     render() {
         return (
             <div className="head">
-                <div className="pedidos">
-                Pedidos
+                <div className="pedidos_serviços">
+                    <div className="label-pedidos">
+                        Pedidos
+                    </div>
+                    <div className="btn-servicos">
+                        <button className="btnServices">Serviços</button>
+                        
+                    </div>
                 </div>
 
                 <div className="actions">
                     <button className="btnAction">Ordenar Por</button>
                     <button className="btnAction">Filtrar Por Serviço</button>
                     <input value="Pesquisar" type="text" className="inputSearch"/>
+                    <div className="pagination-info">
+                    <p>0 de 10</p>
+                    </div>
                 </div>
 
-                {/* <div className="rightElements">
-                    <button className="btnServices">Serviços</button>
-                </div> */}
+              
             </div>
             
         );
